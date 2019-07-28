1a8f818b262f7e014f996cf947dc5345c0b9e90f
pironp <piron.pierre@gmail.com>
2019-07-28 20:36:00 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,52 @@
+var Service, Characteristic;
+const sensor = require('node-dht-sensor');
+
+module.exports = (homebridge) => {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory('homebridge-sensor-dht', 'dht-sensor', DhtSensor);
+};
+
+function DhtSensor(log, config) {
+    this.currentTemperature = 0;
+    this.currentHumidity = 0;
+    this.log = log;
+    this.gpio = config.gpio | 4;
+    this.dhtType = config.dhtType | 11;
+}
+
+DhtSensor.prototype = {
+  getDhtTemperature: function(callback) {
+    sensor.read(this.dhtType, this.gpio, function(err, temperature, humidity) {
+        if (err) {
+           console.log(err);
+           return callback(err);
+        }
+        this.humidityService
+          .getCharacteristic(Characteristic.CurrentRelativeHumidity).updateValue(humidity);
+        return callback(null, temperature);
+    }.bind(this));
+  },
+  getServices: function() {
+    let informationService = new Service.AccessoryInformation();
+
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "dht11")
+      .setCharacteristic(Characteristic.Model, "dht11")
+      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+
+    temperatureService = new Service.TemperatureSensor("Temperature");
+
+    temperatureService
+      .getCharacteristic(Characteristic.CurrentTemperature)
+      .on('get', this.getDhtTemperature.bind(this));
+
+    let humidityService = new Service.HumiditySensor("Humidite");
+
+    this.informationService = informationService;
+    this.temperatureService = temperatureService;
+    this.humidityService = humidityService
+
+    return [this.informationService, this.temperatureService, this.humidityService];
+  }
+};
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Homebridge DHT11-DHT22 Sensor 
+
+### Under development
+
+A homebridge plugin for DHT11/22 sensor.
+
+This plugin is using node-dht-sensor
+
+More to come in the readme
+
+## Debugging
+
+DEBUG=* homebridge -D -P ${PATH_TO_PLUGIN_FOLDER}
